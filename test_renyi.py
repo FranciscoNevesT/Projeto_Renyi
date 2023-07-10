d3d7ca485203a26a240e08b2fa598941d0bdfab4
francisco neves <francisconevest@outlook.com>
2023-07-10 09:05:04 -0300

    Making the Gaussian test more reliably

--- test_renyi.py
+++ test_renyi.py
@@ -4,26 +4,29 @@ from renyi import *
 
 class MyTestCase(unittest.TestCase):
     def test_gaussian(self):
-        num_points = 1000
-        p = 0.8
-        mean = (0,0)
-        conv = [[1,p],
-                [p,1]]
+        dif = []
 
-        points = np.random.multivariate_normal(mean,conv,size=num_points)
+        for _ in range(100):
+            num_points = 1000
+            p = np.random.random()
+            mean = (0,0)
+            conv = [[1,p],
+                    [p,1]]
 
-        ts = tau_s(points,lower_bounds=[-3],upper_bounds=[3])
-        tst = tau_s_t(points,lower_bounds=[-3,-3],upper_bounds=[3,3],bins=10)
+            points = np.random.multivariate_normal(mean,conv,size=num_points)
 
-        metric_renyi = (tst - ts)/tst
-        ref_p = 1 - (1 - p**2)**0.5
+            ts = tau_s(points,lower_bounds=[-3],upper_bounds=[3])
+            tst = tau_s_t(points,lower_bounds=[-3,-3],upper_bounds=[3,3],bins=10)
 
-        print()
+            metric_renyi = (tst - ts)/tst
+            ref_p = 1 - (1 - p**2)**0.5
 
-        print(metric_renyi)
-        print(ref_p)
+            dif.append(abs(metric_renyi - ref_p))
 
-        self.assertLess(abs(metric_renyi - ref_p),0.15)
+        print(np.mean(dif))
+        print(dif)
+
+        self.assertLess(np.mean(dif),0.15)
 
 if __name__ == '__main__':
     unittest.main()
